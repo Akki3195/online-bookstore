89030ccb1abec414ea6fec5987512241e6a95039
Akki3195 <akshaygolam2011@gmail.com>
2019-06-27 02:28:07 +0530

    Store-Front

--- store-front/src/app/components/my-account/my-account.component.ts
+++ store-front/src/app/components/my-account/my-account.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ApplicationInitStatus } from '@angular/core';
+import { AppConst } from 'src/app/constants/app-const';
+import { LoginService } from 'src/app/services/login.service';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-my-account',
   templateUrl: './my-account.component.html',
@@ -6,11 +10,40 @@ import { Component, OnInit, ApplicationInitStatus } from '@angular/core';
 })
 export class MyAccountComponent implements OnInit {
 
-  
+  private serverPath = AppConst.serverPath;
+  private loginError: boolean = false;
+  private loggedIn = false;
+  private credential = {'username' : '','password': ''};
 
-  constructor() { }
+  private emailSent: boolean = false;
+  private userNameExists: boolean;
+  private emailExists: string;
+  private username: string;
+  private email: string;
+
+  private emailNotExists: boolean= false;
+  private forgetPasswordEmailSent: boolean;
+
+  constructor(private loginService: LoginService,
+    private userService: UserService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.loginService.sendCredential(this.credential.username,this.credential.password).subscribe(
+      res => {
+        console.log(res);
+        localStorage.setItem("xAuthToken",res.json().token);
+        this.loggedIn= true;
+        location.reload();
+        this.router.navigate(['/home'])
+      },
+      error => {
+        this.loggedIn = false;
+        this.loginError = true;
+
+      }
+    );
+    
   }
 
 }
--- store-front/src/app/services/login.service.ts
+++ store-front/src/app/services/login.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { AppConst } from '../constants/app-const';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private serverPath : string = AppConst.serverPath;
+  constructor(private http: HttpClient,private router: Router) { }
+
+  sendCredential(username: string,password: string){
+    let url = this.serverPath+'/token';
+    let encodedCredential = btoa(username+":"+password);
+    let basicHeader = "Basic "+encodedCredential;
+    let headers = new HttpHeaders({
+      'Content-Type' : 'application/x-www-form-urlencoded',
+      'Authorization' : basicHeader
+    });
+
+    return this.http.get(url,{headers: headers});
+  }
+
+  checkSession(){
+    let url = this.serverPath+'/checkSession';
+    let headers = new HttpHeaders({
+      'x-auth-token' : localStorage.getItem('xAuthItem')
+    });
+    return this.http.get(url,{headers: headers});
+  }
+
+  logout(){
+    let url = this.serverPath+'/user/logout';
+    let headers = new HttpHeaders({
+      'x-auth-token' : localStorage.getItem('xAuthItem')
+    });
+    return this.http.post(url, '',{headers: headers});
+  }
 }
